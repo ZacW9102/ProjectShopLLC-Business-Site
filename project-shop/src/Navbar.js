127f51237e3cff7f304d0c976fa69162e708608d
ZacW9102 <zacw9102@gmail.com>
2024-09-04 14:05:00 -0700

    navbar styling

--- project-shop/src/Navbar.js
+++ project-shop/src/Navbar.js
@@ -1,10 +1,10 @@
 export default function Navbar() {
     return (
         <div className="navbar">
-            <a href="*">Home</a>
-            <a href="*">Projects</a>
-            <a href="*">Listings</a>
-            <a href="*">Contact</a>
+            <a href="*">HOME</a>
+            <a href="*">PROJECTS</a>
+            <a href="*">LISTINGS</a>
+            <a href="*">CONTACT</a>
         </div>
     )
 }
